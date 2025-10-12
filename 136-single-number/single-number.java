d7a244754f1514bfc6b77a8c02a37b0c7b9d21ef
Kapil Verma <itskapilverma05@gmail.com>
2025-10-12 18:44:51 +0530

    Time: 363 ms (5.51%) | Memory: 46.6 MB (11.20%) - LeetSync

--- 136-single-number/single-number.java
+++ 136-single-number/single-number.java
@@ -1,16 +1,15 @@
 class Solution {
     public int singleNumber(int[] arr) {
-        for(int i=0; i<arr.length; i++){
-        int result = 0;
-            for(int j=0; j<arr.length; j++){
-                if(arr[i]==arr[j]){
-                    result++;
+        int len = arr.length;
+        for (int i = 0; i < len; i++) {
+            int count = 0;
+            for (int j = 0; j < len; j++) {
+                if(arr[i] == arr[j]) {
+                    count++;
                 }
             }
-            if(result==1){
-                return arr[i];
-            }
+            if(count == 1) return arr[i];
         }
-        return -1;
+        return -1; // Error/sentinel
     }
 }
