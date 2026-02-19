db0f5195fb796a256da2027e625db5189108d608
Kapil Verma <itskapilverma05@gmail.com>
2026-02-19 11:03:53 +0530

    Time: 330 ms (65.33%) | Memory: 0B (100.00%) - LeetSync

--- 626-exchange-seats/exchange-seats.sql
+++ 626-exchange-seats/exchange-seats.sql
@@ -1,7 +1,7 @@
 # Write your MySQL query statement below
-SELECT CASE when id % 2 =1 and id+1 in (select id from Seat) then id+1
-            when id % 2 =0 then id-1
-            else id
-        end as id, student
-        from Seat
-        order by id;
+SELECT 
+s1.id,
+(CASE WHEN (s2.student IS NULL) THEN s1.student ELSE s2.student END) AS student 
+FROM Seat s1
+LEFT JOIN Seat s2
+ON (s1.id%2=0 AND s2.id=s1.id-1) OR (s1.id%2!=0 AND s2.id=s1.id+1)
