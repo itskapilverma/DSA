0aeab6a0ffa9747f127253aa51f9c2bbc696461e
Kapil Verma <itskapilverma05@gmail.com>
2026-02-19 11:02:36 +0530

    Time: 344 ms (45.65%) | Memory: 0B (100.00%) - LeetSync

---
+++ 626-exchange-seats/exchange-seats.sql
@@ -0,0 +1,6 @@
+SELECT 
+s1.id,
+(CASE WHEN (s2.student IS NULL) THEN s1.student ELSE s2.student END) AS student 
+FROM Seat s1
+LEFT JOIN Seat s2
+ON (s1.id%2=0 AND s2.id=s1.id-1) OR (s1.id%2!=0 AND s2.id=s1.id+1)
